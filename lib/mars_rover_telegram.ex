ef3a859c39e03c84caa48f2f7c972432eccb4656
Ashton <ashton@platform45.com>
2018-11-18 02:36:25 +0200

    add gpio

--- lib/mars_rover_telegram.ex
+++ lib/mars_rover_telegram.ex
@@ -1,4 +1,5 @@
 defmodule MarsRoverTelegram do
+  alias ElixirALE.GPIO
   @moduledoc """
   Documentation for MarsRoverTelegram.
   """
@@ -14,5 +15,18 @@ defmodule MarsRoverTelegram do
   """
   def hello do
     :world
+  end
+
+  def test do
+    {:ok, pid} = GPIO.start_link(18, :output)
+    IO.puts "Writing to pin 18..."
+    GPIO.write(pid, 1)
+    IO.puts "Done."
+
+    :timer.sleep(1000)
+
+    IO.puts "Writing to pin 18..."
+    GPIO.write(pid, 0)
+    IO.puts "Done."
   end
 end
