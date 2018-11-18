3ee7c91b5f11b6c4ada8e3499d3640875c86698e
Ashton <ashton@platform45.com>
2018-11-18 11:44:40 +0200

    add L R M commands

--- test.py
+++ test.py
@@ -43,10 +43,40 @@ def left_motor_backward():
   time.sleep(1.5)
   GPIO.output(DirectionFourPin, GPIO.LOW)
 
+def l():
+  print("Motor R Direction forward")
+  GPIO.output(DirectionOnePin, GPIO.HIGH)
+  print("Motor L Direction backward")
+  GPIO.output(DirectionFourPin, GPIO.HIGH)
+  time.sleep(1.5)
+  GPIO.output(DirectionOnePin, GPIO.LOW)
+  GPIO.output(DirectionFourPin, GPIO.LOW)
+
+def r():
+  print("Motor R Direction backward")
+  GPIO.output(DirectionTwoPin, GPIO.HIGH)
+  print("Motor L Direction forward")
+  GPIO.output(DirectionThreePin, GPIO.HIGH)
+  time.sleep(1.5)
+  GPIO.output(DirectionTwoPin, GPIO.LOW)
+  GPIO.output(DirectionThreePin, GPIO.LOW)
+
+def m():
+  print("Motor R Direction forward")
+  GPIO.output(DirectionOnePin, GPIO.HIGH)
+  print("Motor L Direction forward")
+  GPIO.output(DirectionThreePin, GPIO.HIGH)
+  time.sleep(1.5)
+  GPIO.output(DirectionOnePin, GPIO.LOW)
+  GPIO.output(DirectionThreePin, GPIO.LOW)
+
 right_motor_forward()
 right_motor_backward()
 left_motor_forward()
 left_motor_backward()
+l()
+r()
+m()
 
 # This command clears the configuration from the GPIO interface
 GPIO.cleanup()
